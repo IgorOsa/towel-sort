b01cfba0f509aaab97175b9a1ea6deed3b2a430f
Igor Osadchyi <igor@webio.net.ua>
2020-10-06 23:06:41 +0300

    feat: implement towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+module.exports = function towelSort(matrix) {
+    function compare(a, b) {
+        return a - b;
+    }
 
-// You should implement your task here.
+    if (!matrix) return [];
 
-module.exports = function towelSort (matrix) {
-  return [];
+    return matrix.reduce((acc, el, i) => {
+        if (el instanceof Array) {
+            return i % 2 === 0
+                ? [...acc, ...el.flat().sort(compare)]
+                : [...acc, ...el.flat().sort(compare).reverse()];
+        }
+    }, []);
 }
